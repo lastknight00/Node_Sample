44ba626596967027efbd0ee02e868aa94c451c37
lastknight00 <darkknight86@hanmail.net>
2016-07-21 20:14:00 +0900

    4.4.2 formidable을 이용한 파일 업로드 처리하기 formidable의 이벤트를 이용하여 파일 업로드를 처리 4.4.3 업로드 진행 상황 계산하기 progress 이벤트를 이용하여 전송된 byte, 총 byte를 전달받아 처리할 수 있다.

---
+++ 2.노드를_이용한_웹_애플리케이션_개발/4.폼에서_사용자_입력받기/fileupload.js
@@ -0,0 +1,66 @@
+var http = require('http');
+var formidable = require('formidable');
+var form = new formidable.IncomingForm();
+
+var server = http.createServer(function(req, res) {
+	switch(req.method) {
+	case 'GET':
+		show(res);
+		break;
+	case 'POST':
+		upload(req, res);
+		break;
+	default:
+		badRequest(res);	
+	}
+});
+
+server.listen(3000);
+
+function show(res) {
+	var html = '<form method = "post" action = "/" enctype = "multipart/form-data">'
+		+ '<p><input type = "text" name = "name" /></p>'
+		+ '<p><input type = "file" name = "file" /></p>'
+		+ '<p><input type = "submit" value = "Upload" /></p>'
+		+ '</form></body></html>';
+	res.setHeader('Content-Type', 'text/html');
+	res.setHeader('Content-Length', Buffer.byteLength(html));
+	res.end(html);
+}
+
+function upload(req, res) {
+	if(!isFormData(req)) {
+		req.statusCode = 400;
+		req.end('Bad Request: expecting multipart/form-data');
+		return;
+	}
+
+	/*form.on('field', function(field, value) {
+		console.log(field);
+		console.log(value);
+	});
+
+	form.on('file', function(name, file) {
+		console.log(name);
+		console.log(file);
+	});
+
+	form.on('end', function() {
+		res.end('upload complete!');
+	});*/
+	
+	form.on('progress', function(bytesReceived, bytesExcepted) {
+		var percent = Math.floor(bytesReceived / bytesExcepted * 100);
+		console.log(percent);
+	});
+	form.parse(req, function(err, fields, files) {
+		console.log(fields);
+		console.log(files);
+		res.end('upload complete');
+	});
+}
+
+function isFormData(req) {
+	var type = req.headers['content-type'] || '';
+	return 0 == type.indexOf('multipart/form-data');
+}
