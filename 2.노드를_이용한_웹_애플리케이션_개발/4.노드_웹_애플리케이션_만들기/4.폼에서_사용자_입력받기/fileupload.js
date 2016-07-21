4ab079c681017f6365efebc59d981a36e4d293ac
lastknight00 <darkknight86@hanmail.net>
2016-07-21 21:25:34 +0900

    파일 위치 변경

--- 2.노드를_이용한_웹_애플리케이션_개발/4.노드_웹_애플리케이션_만들기/4.폼에서_사용자_입력받기/fileupload.js
+++ 2.노드를_이용한_웹_애플리케이션_개발/4.노드_웹_애플리케이션_만들기/4.폼에서_사용자_입력받기/fileupload.js
@@ -1,6 +1,6 @@
 var http = require('http');
 var formidable = require('formidable');
-var form = new formidable.IncomingForm();
+	var form = new formidable.IncomingForm();
 
 var server = http.createServer(function(req, res) {
 	switch(req.method) {
