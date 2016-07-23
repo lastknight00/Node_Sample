d45dfe9fdc8810f3cf9c60b1ba306bddb9a76c7e
lastknight00 <darkknight86@hanmail.net>
2016-07-23 09:06:54 +0900

    5.3.1 레디스 레디스의 채널을 이용한 발행/구독

---
+++ 2.노드를_이용한_웹_애플리케이션_개발/5.노드_애플리케이션_데이터_저장하기/3.NoSQL_데이터베이스/redit/pub_sub.js
@@ -0,0 +1,36 @@
+var net = require('net');
+var redis = require('redis');
+
+var server = net.createServer(function(socket) {
+	var subscriber;
+	var publisher;
+	
+	subscriber = redis.createClient();
+	subscriber.subscribe('main_chat_room');
+	
+	subscriber.on('message', function(channel, message) {
+		socket.write('Channel ' + channel + ': ' + message);
+	});
+	publisher = redis.createClient();
+	socket.on('connect', function() {
+		subscriber = redis.createClient();
+		subscriber.subscribe('main_chat_room');
+		
+		subscriber.on('message', function(channel, message) {
+			socket.write('Channel ' + channel + ': ' + message);
+		});
+		publisher = redis.createClient();
+	});
+	
+	socket.on('data', function(data) {
+		publisher.publish('main_chat_room', data);
+	});
+	
+	socket.on('end', function() {
+		subscriber.unsubscribe('main_chat_room');
+		subscriber.end();
+		publisher.end();
+	});
+});
+
+server.listen(3000);
